00b8f3324e9744f655a8e8e5d83aaeaee14faa73
chungs31 <chungs31@tsagrp-pcseung.ece.utoronto.ca>
2019-09-17 14:01:52 -0400

    parser for weights

---
+++ host/src/importer.cpp
@@ -0,0 +1,51 @@
+#include <iostream> 
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+void weight_parser(const char *filename, vector<vector<float>> &weights) {
+    ifstream infile(filename);
+
+    string line;
+    while (getline(infile, line)) {
+        int num = 0;
+        stringstream ss(line);
+        float weight;
+        char dummy;
+        ss >> dummy;
+        vector<float> curr_weights;
+
+        while (ss >> weight) {
+            curr_weights.push_back(weight);
+            num++;
+            //cout << weight << endl;
+            ss >> dummy;
+        }
+        weights.push_back(curr_weights);
+        cout << num << endl;
+    }
+
+    infile.close();
+}
+
+int main() {
+    vector<vector<float>> weights;
+
+    weight_parser("mnist_weight_dump.txt", weights);
+
+    /*
+    for (auto i : weights) {
+        for (auto j : i) {
+            cout << j << " ";
+        }
+        cout << endl;
+    }
+    */
+
+    return 0;
+}
+
+
