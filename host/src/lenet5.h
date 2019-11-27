beb4f84ae2e942693beb1986d0add79b3fd674cb
chungs31 <chungs31@snail.utoronto.ca>
2019-11-26 19:31:53 -0500

    Adding descriptions to LeNet5

--- host/src/lenet5.h
+++ host/src/lenet5.h
@@ -31,23 +31,23 @@ struct Layer {
 
 namespace LeNet5 {
 
-//const int num_layers = 9;
-const int num_layers = 6;
+const int num_layers = 9; // Default network
 
-Layer nnetwork[] {
+//const int num_layers = 6; // REMEMBER TO CHANGE THIS
+
+Layer network[] { // Default network (Baseline, Unrolled)
     {"fuse_conv2d_relu_kernel0", 5, {676, 784, 54, 4056, 6}, {rw, r, r, w, r}, 3, 1}, 
     {"fuse_avg_pool2d_kernel0", 2, {1014, 4056}, {w, r}, 0, 1}, 
     {"fuse_conv2d_relu_1_kernel0", 5, {121, 1014, 864, 1936, 16}, {w, r, r, w, r}, 3, 1},
     {"fuse_avg_pool2d_1_kernel0", 2, {400, 1924}, {w, r}, 0, 1}, 
     {"fuse_transpose_flatten_kernel0", 2, {400, 400}, {w, r}, 0, 1},
-    //{"fuse_dense_relu_kernel0", 4, {400, 48000, 120, 120}, {r, r, w, r}, 2, 0},
     {"fuse_dense_relu_kernel0", 5, {1, 400, 48000, 120, 120}, {w, r, r, w, r}, 3, 1},
     {"fuse_dense_relu_1_kernel0", 5, {1, 120, 10080, 84, 84}, {w, r, r, w, r}, 3, 1},
     {"fuse_dense_kernel0", 5, {1, 84, 840, 10, 10}, {w, r, r, w, r}, 3, 1},
     {"fuse_softmax_kernel0", 4, {1, 10, 1, 10}, {w, r, w, w}, 3, 1}
 };
 
-Layer channel_network[] {
+Layer channels_network[] { // Channels
     {"fuse_conv2d_relu_kernel0", 3, {784, 54, 6}, {r, r, r}, -1, 0}, 
     {"fuse_avg_pool2d_kernel0", 0, {}, {}, -1, -1}, 
     {"fuse_conv2d_relu_1_kernel0", 2, {864, 16}, {r, r}, -1, -1},
@@ -71,25 +71,13 @@ Layer c_autorun_network[] {
     {"fuse_softmax_kernel0", 1, {10}, {w}, 0, -1}
 };
 
-Layer network[] {
+Layer AUTORUN_network[] { // Channels + Autorun Network
     {"fuse_conv2d_relu_kernel0", 3, {784, 54, 6}, {r, r, r}, -1, 0}, 
     {"fuse_conv2d_relu_1_kernel0", 2, {864, 16}, {r, r}, -1, -1},
     {"fuse_dense_relu_kernel0", 2, {48000, 120}, {r, r}, -1, -1},
     {"fuse_dense_relu_1_kernel0", 2, {10080, 84}, {r, r}, -1, -1},
     {"fuse_dense_kernel0", 2, {840, 10}, {r, r}, -1, -1},
     {"fuse_softmax_kernel0", 1, {10}, {w}, 0, -1}
-};
-
-Layer opta_network[] {
-    {"fuse_conv2d_relu_kernel0", 4, {784, 54, 6, 4056}, {r, r, r, w}, 3, 0}, 
-    {"fuse_avg_pool2d_kernel0", 2, {4056, 1014}, {r, w}, 1, 0}, 
-    {"fuse_conv2d_relu_1_kernel0", 4, {1014, 864, 16, 1936}, {r, r, r, w}, 3, 0},
-    {"fuse_avg_pool2d_1_kernel0", 2, {1924, 400}, {r, w}, 1, 0}, 
-    {"fuse_transpose_flatten_kernel0", 2, {400, 400}, {r, w}, 1, 0},
-    {"fuse_dense_relu_kernel0", 4, {400, 48000, 120, 120}, {r, r, r, w}, 3, 0},
-    {"fuse_dense_relu_1_kernel0", 4, {120, 10080, 84, 84}, {r, r, r, w}, 3, 0},
-    {"fuse_dense_kernel0", 4, {84, 840, 10, 10}, {r, r, r, w}, 3, 0},
-    {"fuse_softmax_kernel0", 2, {10, 10}, {r, w}, 1, 0}
 };
 
 }
